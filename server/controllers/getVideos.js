53cb62b1b4306528025d78c89fc4bd0fc7f542e9
Dave Schumaker <daveschumaker@gmail.com>
2015-08-03 17:05:24 -0700

    Server can now query YouTube API and store video results to a local object.

--- server/controllers/getVideos.js
+++ server/controllers/getVideos.js
@@ -3,6 +3,8 @@
  * with YouTube API... hopefully.
  */
 
+var request = require("request");
+var api = require('../api-config.js');
 
 // Hard coding a number of videos to test out our API.
 // TODO: Replace this with some serious API calls
@@ -20,6 +22,28 @@ var videoObj = [
 
 // Template for adding some fake data for now.
 // { url: 'xxx' },
+// 
+// 
+// https://www.googleapis.com/youtube/v3/search?part=snippet&key=apiKey&q=cats&order=viewCount&maxResults=50&publishedAfter=2015-08-02T00%3A00%3A00Z
+
+// Import our YouTube API key
+var apiKey = api.apiKey();
+
+// Send a GET request to YouTUbe with a query search string
+request("https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + apiKey + "&q=corgis&order=viewCount&maxResults=50&publishedAfter=2015-08-02T00%3A00%3A00Z", function(error, response, body) {
+  var data = JSON.parse(body);
+
+  data.items.forEach(function(item, index) {
+    var videoID = item.id.videoId;
+    var embedID = 'https://www.youtube.com/embed/' + videoID;
+    videoObj.push({url: embedID});
+    console.log('Adding... ', embedID);
+  });
+
+  console.log(data);
+  console.log(data.items[0].id.videoId)
+  // console.log('VIDEO ID: ', JSON.parse(body.items.videoId))
+});
 
 module.exports = {
   getRandom: function() {
